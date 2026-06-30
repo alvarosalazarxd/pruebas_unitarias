8fe9d07f0a5ca8d76da5fa01242f34c5f7055712
alvarosalazarxd <al.salazars@duocuc.cl>
2026-06-29 20:47:40 -0400

    Add files via upload

---
+++ ms_usuarios/src/test/java/com/perfumes/perfumelandia/TestDataFactory.java
@@ -0,0 +1,39 @@
+package com.perfumes.perfumelandia;
+
+import com.perfumes.perfumelandia.ms_usuarios.dto.UsuarioRequestDTO;
+import com.perfumes.perfumelandia.ms_usuarios.model.Usuario;
+
+import net.datafaker.Faker;
+
+public class TestDataFactory {
+
+    private static final Faker faker = new Faker();
+
+  public static Usuario unUsuario() {
+        Usuario u = new Usuario();
+        
+       
+        u.setId(faker.number().numberBetween(1L, 999L));
+        u.setNombre(faker.name().fullName()); 
+        u.setEmail(faker.internet().emailAddress()); 
+        u.setContrasena(faker.internet().password()); 
+        u.setRol("USER"); 
+        u.setActivo(true);
+        u.setSucursal("Sucursal Central");
+        
+        return u;
+    }
+
+
+    public static UsuarioRequestDTO unUsuarioRequest() {
+        UsuarioRequestDTO dto = new UsuarioRequestDTO();
+        dto.setNombre(faker.name().username());
+        return dto;
+    }
+
+    public static Usuario crearUsuarioAleatorio() {
+        
+        return unUsuario(); 
+    }
+    
+}
