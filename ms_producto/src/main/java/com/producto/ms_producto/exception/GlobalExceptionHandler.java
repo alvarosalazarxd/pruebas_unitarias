d9cc38cea6d7d6bd6273a275fe7c87d0217ba7ab
alvarosalazarxd <al.salazars@duocuc.cl>
2026-06-30 04:49:15 -0400

    Add files via upload

---
+++ ms_producto/src/main/java/com/producto/ms_producto/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.producto.ms_producto.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(
+            ResourceNotFoundException.class)
+
+    public ResponseEntity<Map<String, Object>>
+    manejarNotFound(
+            ResourceNotFoundException ex) {
+
+        return ResponseEntity.status(
+                HttpStatus.NOT_FOUND)
+
+                .body(Map.of(
+                        "timestamp",
+                        LocalDateTime.now(),
+
+                        "mensaje",
+                        ex.getMessage()
+                ));
+    }
+}
